c717d778201e75bf82d2507240d1f496be2d377b
utn203241073_GitHub <203241073@alumnos.utn.edu.mx_GitHub>
2022-04-05 21:26:57 -0500

    amy

---
+++ src/main/java/com/amy/diaz/negrete/repository/CategoriasRepository.java
@@ -0,0 +1,11 @@
+package com.amy.diaz.negrete.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.amy.diaz.negrete.model.Categoria;
+
+
+//public interface CategoriasRepository extends CrudRepository<Categoria, Integer> 
+public interface CategoriasRepository extends JpaRepository<Categoria, Integer>{
+
+}
---
+++ src/main/java/com/amy/diaz/negrete/repository/VacantesRepository.java
@@ -0,0 +1,8 @@
+package com.amy.diaz.negrete.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.amy.diaz.negrete.model.Vacante;
+
+public interface VacantesRepository extends JpaRepository<Vacante, Integer> {
+
+}
